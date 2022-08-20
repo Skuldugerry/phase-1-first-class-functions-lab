9fe723ea9e9a9d7f193e6cd8a2860cb6d7688de1
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-20 20:37:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+    return [drivers[0],
+            drivers[1]];
+}
+
+const returnLastTwoDrivers= function(drivers){
+    return [drivers[drivers.length-2],
+            drivers[drivers.length-1]];
+}
+let selectingDrivers=[returnFirstTwoDrivers,
+                    returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare*number};
+}
+
+const fareDoubler=function(createFareMultiplier){
+    return createFareMultiplier*2;
+}
+
+const fareTripler=function(createFareMultiplier){
+    return createFareMultiplier*3;
+}
+
+function selectDifferentDrivers(drivers,choice){
+    return choice===returnFirstTwoDrivers?(returnFirstTwoDrivers(drivers)):(returnLastTwoDrivers(drivers));
+
+}
